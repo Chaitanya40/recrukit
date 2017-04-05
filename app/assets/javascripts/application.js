e0619a0e9d8b68469507a68dd93002d939742a4b
Chaitanya40 <chaitanyaallu1605@gmail.com>
2017-04-05 22:31:21 +0530

    Initialize repo

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,37 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.remotipart
+//= require_tree
+//= require bootstrap-sprockets
+//= require metisMenu/jquery.metisMenu.js
+//= require pace/pace.min.js
+//= require slimscroll/jquery.slimscroll.min.js
+//= require inspinia.js
+//= require underscore_min/underscore-min.js
+//= require toastr/toastr.min.js
+//= require jquery-ui.min.js
+
+
+
+
+
+
+//= require iCheck/icheck.min.js
+//= require colorpicker/bootstrap-colorpicker.min.js
+//= require cropper/cropper.min.js
+//= require datapicker/bootstrap-datepicker.js
+//= require switchery/switchery.js
+//= require chosen/chosen.jquery.js
+//= require jasny/jasny-bootstrap.min.js
